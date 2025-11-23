46d000ce66192f9542c0edcc22ba75036b5cc4ee
Matias Farias Valdenegro <matiasfvaldenegro@gmail.com>
2025-11-22 23:46:26 -0300

    se agrega parametros para pactos electorales

---
+++ functions/parameters.py
@@ -0,0 +1,108 @@
+pacto_map = {
+    # ----------------------------------------------------------------------
+    # A - PARTIDO ECOLOGISTA VERDE
+    # ----------------------------------------------------------------------
+
+    "PEV": "A - PARTIDO ECOLOGISTA VERDE",
+
+    # ----------------------------------------------------------------------
+    # B - VERDES, REGIONALISTAS Y HUMANISTAS
+    # ----------------------------------------------------------------------
+    "AH": "B - VERDES, REGIONALISTAS Y HUMANISTAS",
+    "IND - AH": "B - VERDES, REGIONALISTAS Y HUMANISTAS",
+    "FREVS": "B - VERDES, REGIONALISTAS Y HUMANISTAS",
+    "IND - FREVS": "B - VERDES, REGIONALISTAS Y HUMANISTAS",
+
+    # ----------------------------------------------------------------------
+    # C - UNIDAD POR CHILE
+    # ----------------------------------------------------------------------
+    "PL": "C - UNIDAD POR CHILE",
+    "IND - PL": "C - UNIDAD POR CHILE",
+
+    "FA": "C - UNIDAD POR CHILE",
+    "IND - FA": "C - UNIDAD POR CHILE",
+
+    "PS": "C - UNIDAD POR CHILE",
+    "IND - PS": "C - UNIDAD POR CHILE",
+
+    "PDC": "C - UNIDAD POR CHILE",
+    "IND - PDC": "C - UNIDAD POR CHILE",
+
+    "PPD": "C - UNIDAD POR CHILE",
+    "IND - PPD": "C - UNIDAD POR CHILE",
+
+    "PCCH": "C - UNIDAD POR CHILE",
+    "IND - PCCH": "C - UNIDAD POR CHILE",
+
+    "PR": "C - UNIDAD POR CHILE",
+    "IND - PR": "C - UNIDAD POR CHILE",
+
+    "DEMOCRATAS": "C - UNIDAD POR CHILE",
+    "IND - DEMOCRATAS": "C - UNIDAD POR CHILE",
+
+    # ----------------------------------------------------------------------
+    # D - IZQUIERDA ECOLOGISTA POPULAR ANIMALISTA Y HUMANISTA
+    # ----------------------------------------------------------------------
+    "PH": "D - IZQUIERDA ECOLOGISTA POPULAR ANIMALISTA Y HUMANISTA",
+    "IND - PH": "D - IZQUIERDA ECOLOGISTA POPULAR ANIMALISTA Y HUMANISTA",
+    "IGUALDAD": "C - UNIDAD POR CHILE",
+    "IND - IGUALDAD": "C - UNIDAD POR CHILE",
+
+    # ----------------------------------------------------------------------
+    # E - MOVIMIENTO AMARILLOS POR CHILE
+    # ----------------------------------------------------------------------
+
+    "AMARILLOS": "E - MOVIMIENTO AMARILLOS POR CHILE",
+
+    # ----------------------------------------------------------------------
+    # F - PARTIDO DE TRABAJADORES REVOLUCIONARIOS
+    # ----------------------------------------------------------------------
+    "PTR": "F - PARTIDO DE TRABAJADORES REVOLUCIONARIOS",
+
+    # ----------------------------------------------------------------------
+    # G - PARTIDO ALIANZA VERDE POPULAR
+    # ----------------------------------------------------------------------
+    "PAVP": "G - PARTIDO ALIANZA VERDE POPULAR",
+
+    # ----------------------------------------------------------------------
+    # H - PARTIDO ALIANZA VERDE POPULAR
+    # ----------------------------------------------------------------------
+
+    "POPULAR": "H - POPULAR",
+
+    # ----------------------------------------------------------------------
+    # I - PARTIDO DE LA GENTE
+    # ----------------------------------------------------------------------
+    "PDG": "I - PARTIDO DE LA GENTE",
+    "IND - PDG": "I - PARTIDO DE LA GENTE",
+
+    # ----------------------------------------------------------------------
+    # J - CHILE GRANDE Y UNIDO
+    # ----------------------------------------------------------------------
+    "UDI": "J - CHILE GRANDE Y UNIDO",
+    "IND - UDI": "J - CHILE GRANDE Y UNIDO",
+
+    "RN": "J - CHILE GRANDE Y UNIDO",
+    "IND - RN": "J - CHILE GRANDE Y UNIDO",
+
+    "EVOPOLI": "J - CHILE GRANDE Y UNIDO",
+    "IND - EVOPOLI": "J - CHILE GRANDE Y UNIDO",
+
+    # ----------------------------------------------------------------------
+    # K - CAMBIO POR CHILE
+    # ----------------------------------------------------------------------
+    "REPUBLICAN": "K - CAMBIO POR CHILE",
+    "IND - REPUBLICAN": "K - CAMBIO POR CHILE",
+
+    "PSC": "K - CAMBIO POR CHILE",
+    "IND - PSC": "K - CAMBIO POR CHILE",
+
+    "PNL": "K - CAMBIO POR CHILE",
+    "IND - PNL": "K - CAMBIO POR CHILE",
+
+    # ----------------------------------------------------------------------
+    # CANDIDATURA INDEPENDIENTE
+    # ----------------------------------------------------------------------
+    "IND": "CANDIDATURA INDEPENDIENTE"
+
+}
